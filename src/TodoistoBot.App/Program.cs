e49db4aa7eede2e6174cc74d747a8f2d655a916f
Paul Buzakov <buzakovpa@gmail.com>
2022-07-16 11:26:04 +0500

    * Update type of App

---
+++ src/TodoistoBot.App/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+
+
+namespace TodoistoBot.App;
+
+public static class Program
+{
+    private static Task Main(string[] args) => new HostBuilder()
+        .ConfigureServices((context, services) =>
+        {
+        })
+        .RunConsoleAsync();
+}
